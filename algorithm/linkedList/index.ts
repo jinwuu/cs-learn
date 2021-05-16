e66bb59aa7ba9007d0a03b89a17ba9852e33f437
warlock <20466971@qq.com>
2021-05-16 22:38:00 +0800

    add deleteNode

--- algorithm/linkedList/index.ts
+++ algorithm/linkedList/index.ts
@@ -167,3 +167,9 @@ function isPalindrome(head: ListNode): Boolean {
   }
   return true
 }
+
+// 237. 删除链表中的节点: 编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为 要被删除的节点 。
+function deleteNode(node: ListNode) {
+  node.val = node.next.val
+  node.next = node.next.next
+}
