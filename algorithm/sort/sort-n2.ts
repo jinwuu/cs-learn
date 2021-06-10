7d9153d7936260a737d09e1f0a90e7530b88f28f
warlock <20466971@qq.com>
2021-06-10 21:02:40 +0800

    add shellSort

--- algorithm/sort/sort-n2.ts
+++ algorithm/sort/sort-n2.ts
@@ -31,6 +31,24 @@ function insertSort(arr) {
   return arr
 }
 
+// 希尔
+function shellSort(arr) {
+  let h = Math.pow(2, ~~Math.log2(arr.length + 2) - 1) - 1 // Math.pow(2, k) - 1
+  while (h >= 1) {
+    for (let i = h; i < arr.length; i++) {
+      let p = i
+      let temp = arr[p]
+      for (let j = i - h; j >= 0 && arr[j] > temp; j -= h) {
+        arr[j + h] = arr[p = j]
+      }
+      if (i !== p)
+        arr[p] = temp
+    }
+    h = (h + 1) / 2 - 1
+  }
+  return arr
+}
+
 // 选择
 function selectionSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
